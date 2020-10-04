f7712ae4b119504e8f0782569e46f1b9c9046b3a
Jeffrey-Wang <17jwapples@gmail.com>
2020-10-04 15:49:15 -0400

    Arrows can deal damage

---
+++ Medieval Jam/Assets/Scripts/Interface/IDamageable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageable
+{
+    // Damages the entity. Returns true if it "died".
+    bool DealDamage(GameObject src, float damage);
+}
--- Medieval Jam/Assets/Scripts/TowerScript.cs
+++ Medieval Jam/Assets/Scripts/TowerScript.cs
@@ -6,6 +6,7 @@ public class TowerScript : MonoBehaviour
 {
     public GameObject m_TowerArrowPrefab;
     public GameObject m_fireOrigin;
+    public float m_damage = 15f;
     public float m_fireRate = 0.2f;
     public float m_fireVelocity = 10f;
     private float m_fireCooldown;
@@ -28,8 +29,13 @@ public class TowerScript : MonoBehaviour
     private void Fire() {
         GameObject newArrow_GO = Instantiate(m_TowerArrowPrefab, m_fireOrigin.transform.position, m_fireOrigin.transform.rotation);
         Rigidbody newArrow_R = newArrow_GO.GetComponent<Rigidbody>();
-        if (newArrow_R) {
+        if (newArrow_R != null) {
             newArrow_R.velocity = newArrow_GO.transform.forward * m_fireVelocity;
+        }
+        TowerArrowScript newArrow_S = newArrow_GO.GetComponent<TowerArrowScript>();
+        if (newArrow_S != null) {
+            newArrow_S.m_damage = m_damage;
+            newArrow_S.m_src = gameObject;
         }
     }
 }
